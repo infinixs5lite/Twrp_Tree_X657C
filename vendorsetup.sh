97e9381dd80a4737baf504908a2117d8197b79be
Miracle <127039117+Miraclev1@users.noreply.github.com>
2023-08-05 22:30:16 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X657C-user
-add_lunch_combo omni_Infinix-X657C-userdebug
-add_lunch_combo omni_Infinix-X657C-eng
+add_lunch_combo twrp_Infinix-X657C-user
+add_lunch_combo twrp_Infinix-X657C-userdebug
+add_lunch_combo twrp_Infinix-X657C-eng
