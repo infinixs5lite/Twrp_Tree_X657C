0e55dcbe3bf31d2c339c7741973b9b631e7c0216
Miracle <127039117+Miraclev1@users.noreply.github.com>
2023-08-05 21:17:40 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,4 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/infinix-X657C
+LOCAL_PATH := device/infinix/X657C
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Api level
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# VNDK version
+PRODUCT_TARGET_VNDK_VERSION := 29
+
+# fastbootd hal
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
